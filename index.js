1deac9206a2f9b772f505308b75c2d5431a2383b
mcwar0501 <luadao5962@gmail.com>
2025-03-27 22:12:31 +0700

    45p

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ const sendNegativeValuesToTelegram = async (negativeValues) => {
 let isFetching = false;
 
 import cron from "node-cron";
-cron.schedule("15 * * * *", async () => {
+cron.schedule("45 * * * *", async () => {
     console.log("🔄 Đang chạy vào", new Date().toLocaleTimeString());
 
     await fetchAllFundingRates();
